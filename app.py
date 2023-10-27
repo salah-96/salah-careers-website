7e258700851e1f412136c0f15c9e0b212338e1ab
salah-96 <146073941+salah-96@users.noreply.github.com>
2023-10-27 17:09:57 +0000

    added  HTML and css

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from abc import abstractproperty
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-  return "hello,salah"
+def hello_Salah():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
